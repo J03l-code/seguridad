e07f277f90245a087e0e4f425e79ec9faa4ae18e
J03l-code <joeljiyane@gmail.com>
2026-03-30 20:00:40 -0500

    chore: enable api display_errors temporarily to catch Hostinger 500 stack trace

--- api/calendar_events.php
+++ api/calendar_events.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/helpers.php';
 require_once __DIR__ . '/google_calendar_helper.php';
