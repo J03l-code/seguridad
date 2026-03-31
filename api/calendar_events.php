225824b1ed9b5f23f2bb65bc42d4da50ea277450
J03l-code <joeljiyane@gmail.com>
2026-03-30 20:07:06 -0500

    chore: implement global JSON crash handler to intercept hidden Hostinger PHP fatal errors

--- api/calendar_events.php
+++ api/calendar_events.php
@@ -1,7 +1,13 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+register_shutdown_function(function () {
+    $error = error_get_last();
+    if ($error && in_array($error['type'], [E_ERROR, E_CORE_ERROR, E_COMPILE_ERROR, E_USER_ERROR, E_RECOVERABLE_ERROR, E_PARSE])) {
+        http_response_code(500);
+        header('Content-Type: application/json');
+        echo json_encode(['error' => "CRASH: " . $error['message'] . " in " . basename($error['file']) . ":" . $error['line']]);
+        exit;
+    }
+});
 
 require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/helpers.php';
